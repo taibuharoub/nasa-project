96c783b724c03e9466e4a942cba17696af387f1d
Taibu Haroub <haroubtaibu@gmail.com>
2021-12-02 00:03:41 +0300

    Add Logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 const exp = require("constants");
@@ -11,6 +12,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
