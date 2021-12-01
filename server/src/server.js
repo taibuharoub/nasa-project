bc36351be4bc5053191a665d2968677bece08941
Taibu Haroub <haroubtaibu@gmail.com>
2021-12-01 20:25:50 +0300

    Get all Planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,19 @@
 const http = require("http");
 const app = require("./app");
+
+const { loadPLanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
-});
+
+// load planets data on server start (before server starts listening)
+async function startServer() {
+  await loadPLanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+  });
+}
+
+startServer();
