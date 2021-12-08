3532d0d7ac498603883296c274ada363d70e789c
Taibu Haroub <haroubtaibu@gmail.com>
2021-12-08 22:51:58 +0300

    Connect to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,25 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 const { loadPLanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URI = "";
+
 const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MonogDB connection ready!");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 
 // load planets data on server start (before server starts listening)
 async function startServer() {
+  await mongoose.connect(MONGO_URI);
   await loadPLanetsData();
 
   server.listen(PORT, () => {
